8f2564b1bd54f3d95bd7f1f16d545b0390b8710e
Andres <andres.edu.glez@gmail.com>
2026-02-10 12:24:26 -0600

    h2

--- db/init_auth.sh
+++ db/init_auth.sh
@@ -5,11 +5,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     
-    -- 1. Crear usuario Admin (ang)
+    -- 1. Crear usuario Admin 
     CREATE USER ang WITH PASSWORD '$DB_USER_PASSWORD';
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ang;
 
-    -- 2. Crear usuario App (seguro)
+    -- 2. Crear usuario App 
     DROP ROLE IF EXISTS app;
     CREATE ROLE app WITH LOGIN PASSWORD '$DB_APP_PASSWORD';
 
@@ -21,7 +21,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM app;
     REVOKE ALL ON ALL TABLES IN SCHEMA public FROM app;
 
-    -- 5. Dar permisos SOLO a las vistas (Requisito examen)
+    -- 5. Dar permisos SOLO a las vistas 
     GRANT SELECT ON vw_course_performance TO app;
     GRANT SELECT ON vw_teacher_load TO app;
     GRANT SELECT ON vw_students_at_risk TO app;
